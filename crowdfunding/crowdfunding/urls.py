ad97b03e61e00a9d739c71b738f5b4bc7348c0d3
Clahryott <ck.truscott@gmail.com>
2023-01-21 12:08:13 +0800

    removed notation on urlpattern

--- crowdfunding/crowdfunding/urls.py
+++ crowdfunding/crowdfunding/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('admin/', admin.site.urls), ###
+    path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('users/', include('users.urls')),  
     path('', include('projects.urls')),
