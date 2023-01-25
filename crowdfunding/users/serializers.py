d67242aff04988de8d0fd71756b34c143bb11cd0
Clahryott <ck.truscott@gmail.com>
2023-01-25 16:44:29 +0800

    deploy setting up Part1

--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -6,6 +6,8 @@ class CustomUserSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=150)
     email = serializers.EmailField()
 
+    # ADD password creation / don't worry about password reset
+
     def create(self, validated_data):
         return CustomUser.objects.create(**validated_data)  # validated_data means...?
 
